c082ce8e2130e181b81a373fa6514e47b39af423
GuillaumeLoriot <loriot.guillaume@gmail.com>
2025-05-22 22:02:32 +0200

    made fileuploader service, paginator service, hashpassword listener, made register logic with form and started its view. added image entity and gallery logic for user + adapted fixtures.  inproved views

---
+++ src/Service/FileUploader.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class FileUploader
+{
+
+    // private string $targetDirectory;
+
+    public function __construct(
+        private SluggerInterface $slugger,
+    ) {
+        // $this->targetDirectory = $targetDirectory;
+    }
+
+    // ce service peut faire de l'upload de fichier mais dans ce projet, il ne sera utilisé que pour de l'upload d'images
+
+      /**
+     * Upload un fichier dans un dossier donné.
+     *
+     * @param UploadedFile $file Le fichier à uploader
+     * @param string $targetDirectory Le dossier cible d'upload
+     * @return string Le nom du fichier sauvegardé
+     */
+    public function upload(string $targetDirectory, UploadedFile $file): string
+    {
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($originalFilename);
+        $fileName = $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
+
+        try {
+            $file->move($targetDirectory, $fileName);
+            
+        } catch (FileException $e) {
+            throw new \Exception('Erreur lors du chargement du fichier.');
+        }
+
+        return $fileName;
+    }
+
+    // public function getTargetDirectory(): string
+    // {
+    //     return $this->targetDirectory;
+    // }
+}
